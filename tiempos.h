8054432702732f985591a3f184497733d228de46
DiegoGCV <73501708+DiegoGCV@users.noreply.github.com>
2020-10-26 22:22:01 -0600

    Arreglo CIS 190
    Algunas líneas contenían más de 80 caracteses

--- tiempos.h
+++ tiempos.h
@@ -4,14 +4,15 @@
 
 class tiempos
 {
-public:
-    int llegada;        //Tiempo en el que el proceso llega a "Listo".
-    int finalizacion;   //Tiempo en el que el proceso termina.
-    int retorno;        //Tiempo en el que el proceso estuvo en memoria.
-    int respuesta = -1; //Tiempo en el que el programa tardo en despachar al proceso por primera vez.
-    int espera;         //Tiempo en el que el proceso no estuvo en ejecución.
-    int servicio;       //Tiempo en el que el proceso estuvo en ejecución.
-    tiempos();
+    public:
+        int llegada;        //Tiempo en el que el proceso llega a "Listo".
+        int finalizacion;   //Tiempo en el que el proceso termina.
+        int retorno;        //Tiempo en el que el proceso estuvo en memoria.
+        int respuesta = -1; //Tiempo en el que el programa tardo en despachar 
+                            //al proceso por primera vez.
+        int espera;         //Tiempo en el que el proceso no estuvo en ejecución.
+        int servicio;       //Tiempo en el que el proceso estuvo en ejecución.
+        tiempos();  
 };
 
 #endif // TIEMPOS_H
